5c5affa008a4da62c7c3b2d1ea0f78715952b45b
irfandoart1626 <irfandoirfando44@gmail.com>
2025-05-24 16:49:00 +0700

    feat:api keep alive

--- app/keepalive_task.py
+++ app/keepalive_task.py
@@ -4,7 +4,7 @@ import httpx
 from app.config import Config
 
 KEEP_ALIVE_URL = "https://chatbot-finance-production.up.railway.app/keep-alive"
-INTERVAL = 3  # Detik
+INTERVAL = 1  # Detik
 
 async def keep_alive_task():
     """
